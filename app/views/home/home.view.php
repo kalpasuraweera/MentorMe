df7ee4452c5573047bd984ef9009efd3b2b4ad29
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-07-20 22:07:41 +0530

    add css files and sample classes

--- app/views/home/home.view.php
+++ app/views/home/home.view.php
@@ -1,1 +1,18 @@
-<h1>Home Page</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MentorMe</title>
+    <link rel="stylesheet" href="http://localhost/mentorme/public/css/index.css">
+</head>
+
+<body>
+    <div class="flex">
+        <h1 class="text-primary">Home Page</h1>
+        <p class="text-xs mt-0 sm:font-bold">Welcome to MentorMe</p>
+    </div>
+</body>
+
+</html>
