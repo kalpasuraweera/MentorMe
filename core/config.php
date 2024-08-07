f623e563555f559d96b1ee3a03b194545be4902e
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-08-08 00:52:14 +0530

    Add database configuration and connection functionality

--- core/config.php
+++ core/config.php
@@ -1,2 +1,7 @@
 <?php
+define('DEBUG', true);
 define('BASE_URL', 'http://localhost/mentorme');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mentorme_db');
