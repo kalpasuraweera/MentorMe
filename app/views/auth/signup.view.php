f10b8064d8185563437ba1d45af6687557d4acf4
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-08-09 00:45:02 +0530

    fix spacing css

--- app/views/auth/signup.view.php
+++ app/views/auth/signup.view.php
@@ -1,39 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MentorMe</title>
     <link rel="stylesheet" href="<?= BASE_URL ?>/public/css/index.css">
 </head>
+
 <body class="bg-primary">
-    <div class="flex justify-center items-center flex-wrap w-full h-70p mt-10p">
-        <div class="bg-white rounded-lg margin-auto flex flex-col items-center py-7 px-2_5">
-            <img src="<?= BASE_URL ?>/public/images/superVisor.png" alt="Your Image" class="block max-w-full h-auto mx-auto">
+    <div class="flex justify-center items-center flex-wrap w-full h-screen">
+        <div class="bg-white rounded-lg margin-auto flex flex-col items-center py-10 px-2">
+            <img src="<?= BASE_URL ?>/public/images/superVisor.png" alt="Your Image"
+                class="block max-w-full h-auto mx-auto">
             <h1 class="text-indigo-900 text-center">Supervisor</h1>
             <p class="text-secondary text-center">Monitor projects and groups you are supervising easily</p>
-            <div class="bg-indigo-600 h-10 px-6 rounded-3xl justify-start items-center gap-2 inline-flex">
-                <div class="pr-1.5 justify-start items-center flex">
+            <div class="bg-indigo-600 h-10 px-5 rounded-3xl justify-start items-center gap-2 inline-flex">
+                <div class="pr-2 justify-start items-center flex">
                     <div class="text-white text-xl font-medium font-['DM Sans'] leading-10">
-                    <button >Button</button>                          </div>
+                        <button>Button</button>
+                    </div>
                 </div>
             </div>
 
         </div>
 
-        <div class="bg-white rounded-lg margin-auto flex flex-col items-center py-7 px-2_5">
-            <img src="<?= BASE_URL ?>/public/images/superVisor.png" alt="Your Image" class="block max-w-full h-auto mx-auto">
+        <div class="bg-white rounded-lg margin-auto flex flex-col items-center py-10 px-2">
+            <img src="<?= BASE_URL ?>/public/images/superVisor.png" alt="Your Image"
+                class="block max-w-full h-auto mx-auto">
             <h1 class="text-indigo-900 text-center">Examiner</h1>
-            <p class="text-secondary text-center">Examine  projects and groups you are marking easily</p>
-            
-            <div class="bg-indigo-600 h-10 px-6 rounded-3xl justify-start items-center gap-2 inline-flex">
-                <div class="pr-1.5 justify-start items-center flex">
+            <p class="text-secondary text-center">Examine projects and groups you are marking easily</p>
+
+            <div class="bg-indigo-600 h-10 px-5 rounded-3xl justify-start items-center gap-2 inline-flex">
+                <div class="pr-2 justify-start items-center flex">
                     <div class="text-white text-xl font-medium font-['DM Sans'] leading-10">
-                    <button >Button</button>                          </div>
+                        <button>Button</button>
+                    </div>
                 </div>
             </div>
         </div>
 
     </div>
 </body>
+
 </html>
