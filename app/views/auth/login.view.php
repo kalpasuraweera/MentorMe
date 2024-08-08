3ef1eb2cd1a4fe892b6780f7ebfe35c992a50b03
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-08-08 13:19:24 +0530

    create login page

--- app/views/auth/login.view.php
+++ app/views/auth/login.view.php
@@ -1,1 +1,49 @@
-<h1>Login Page</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MentorMe</title>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/public/css/index.css">
+</head>
+
+<body>
+    <div class="flex items-center h-screen bg-primary">
+        <div class="flex flex-col px-5">
+            <div class="">
+                <h1 class="text-indigo-900 text-6xl font-bold my-5">MentorMe</h1>
+                <p class="text-gray-500 text-base font-medium my-5">
+                    As a Clinical Research Coordinator, you will be responsible for managing and coordinating clinical
+                    trials and research studies. You will work closely with principal investigators, research staff, and
+                    study participants to ensure the smooth operation of research projects.
+                </p>
+
+            </div>
+
+            <form class="px-10 py-10 bg-white rounded-2xl flex-col justify-start items-start">
+                <div class="flex flex-col mt-5">
+                    <label for="email" class="">Email</label>
+                    <input
+                        class="px-4 py-2 bg-white rounded-lg border border-primary justify-start items-center inline-flex"
+                        id="email" type="email" name="email">
+                </div>
+                <div class="flex flex-col my-5">
+                    <label for="password" class="">Password</label>
+                    <input
+                        class="px-4 py-2 bg-white rounded-lg border border-primary justify-start items-center inline-flex"
+                        id="password" type="password" name="password">
+                </div>
+                <div class="flex justify-end">
+                    <button
+                        class="btn-bg-primary rounded-3xl text-center text-white text-base font-medium px-10 py-2">Login</button>
+                </div>
+            </form>
+        </div>
+        <div class="">
+            <img src="<?= BASE_URL ?>/public/images/login_img.png" alt="login page image">
+        </div>
+    </div>
+</body>
+
+</html>
