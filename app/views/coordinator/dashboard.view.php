2c98131a937fa61a74f534e50906c1ce2cd4fa01
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-08-13 23:22:07 +0530

    create side bar

--- app/views/coordinator/dashboard.view.php
+++ app/views/coordinator/dashboard.view.php
@@ -9,17 +9,49 @@
 </head>
 
 <body>
-    <div class="flex flex-row">
-        <div class="flex flex-col w-1/4 bg-indigo-900 text-white">
+    <div class="flex flex-row bg-primary-color h-screen">
+        <div class="flex flex-col w-1/4 text-white bg-white">
             <div class="flex flex-col items-center p-5">
                 <img src="<?= BASE_URL ?>/public/images/dashboard_logo.png" alt="dashboard logo">
             </div>
-            <div class="flex flex-col items-center">
+            <div class="flex flex-col items-center p-5 gap-2">
+                <a href="<?= BASE_URL ?>/coordinator/dashboard"
+                    class="flex flex-row items-center w-full mx-10 btn-primary-color text-white rounded-md">
+                    <img src="<?= BASE_URL ?>/public/images/icons/dashboard_primary.png" alt="dashboard icon"
+                        class="mx-2">
+                    <p class="ml-2">Dashboard</p>
+                </a>
+                <a href="<?= BASE_URL ?>/coordinator/dashboard"
+                    class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
+                    <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
+                        class="mx-2">
+                    <p class="ml-2">Manage Students</p>
+                </a>
+                <a href="<?= BASE_URL ?>/coordinator/dashboard"
+                    class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
+                    <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
+                        class="mx-2">
+                    <p class="ml-2">Manage Supervisors</p>
+                </a>
+                <a href="<?= BASE_URL ?>/coordinator/dashboard"
+                    class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
+                    <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
+                        class="mx-2">
+                    <p class="ml-2">Manage Groups</p>
+                </a>
+                <a href="<?= BASE_URL ?>/coordinator/dashboard"
+                    class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
+                    <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
+                        class="mx-2">
+                    <p class="ml-2">Manage Examiners</p>
+                </a>
+                <a href="<?= BASE_URL ?>/coordinator/dashboard"
+                    class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
+                    <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
+                        class="mx-2">
+                    <p class="ml-2">Calendar</p>
+                </a>
 
-                <a href="<?= BASE_URL ?>/coordinator/dashboard" class="p-4 hover:bg-indigo-800">Dashboard</a>
-                <a href="<?= BASE_URL ?>/coordinator/students" class="p-4 hover:bg-indigo-800">Students</a>
-                <a href="<?= BASE_URL ?>/coordinator/feedbacks" class="p-4 hover:bg-indigo-800">Feedbacks</a>
-                <a href="<?= BASE_URL ?>/coordinator/logout" class="p-4 hover:bg-indigo-800">Logout</a>
             </div>
         </div>
         <div class="flex flex-col w-3/4 p-5">
