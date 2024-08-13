13a4903e63c0666798d388fed06dd5daa00dea81
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-08-14 00:34:28 +0530

    Update CSS to remove margin and padding for paragraphs

--- app/views/coordinator/dashboard.view.php
+++ app/views/coordinator/dashboard.view.php
@@ -19,48 +19,71 @@
                     class="flex flex-row items-center w-full mx-10 btn-primary-color text-white rounded-md">
                     <img src="<?= BASE_URL ?>/public/images/icons/dashboard_primary.png" alt="dashboard icon"
                         class="mx-2">
-                    <p class="ml-2">Dashboard</p>
+                    <p class="ml-2 py-4">Dashboard</p>
+                </a>
+                <a href="<?= BASE_URL ?>/coordinator/students"
+                    class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
+                    <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
+                        class="mx-2">
+                    <p class="ml-2 py-4">Manage Students</p>
                 </a>
                 <a href="<?= BASE_URL ?>/coordinator/dashboard"
                     class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
                     <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
                         class="mx-2">
-                    <p class="ml-2">Manage Students</p>
+                    <p class="ml-2 py-4">Manage Supervisors</p>
                 </a>
                 <a href="<?= BASE_URL ?>/coordinator/dashboard"
                     class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
                     <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
                         class="mx-2">
-                    <p class="ml-2">Manage Supervisors</p>
+                    <p class="ml-2 py-4">Manage Groups</p>
                 </a>
                 <a href="<?= BASE_URL ?>/coordinator/dashboard"
                     class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
                     <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
                         class="mx-2">
-                    <p class="ml-2">Manage Groups</p>
+                    <p class="ml-2 py-4">Manage Examiners</p>
                 </a>
                 <a href="<?= BASE_URL ?>/coordinator/dashboard"
                     class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
                     <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
                         class="mx-2">
-                    <p class="ml-2">Manage Examiners</p>
-                </a>
-                <a href="<?= BASE_URL ?>/coordinator/dashboard"
-                    class="flex flex-row items-center w-full mx-10 text-secondary-color rounded-md hover:btn-primary-color hover:text-white">
-                    <img src="<?= BASE_URL ?>/public/images/icons/dashboard_secondary.png" alt="dashboard icon"
-                        class="mx-2">
-                    <p class="ml-2">Calendar</p>
+                    <p class="ml-2 py-4">Calendar</p>
                 </a>
 
             </div>
         </div>
         <div class="flex flex-col w-3/4 p-5">
             <div class="flex justify-between items-center">
-                <h1 class="text-3xl font-bold">Coordinator Dashboard</h1>
-                <a href="<?= BASE_URL ?>/coordinator/logout"
-                    class="p-2 btn-primary-color text-white rounded-lg">Logout</a>
+                <h1 class="text-3xl font-bold text-primary-color">Coordinator Dashboard</h1>
+                <div class="flex flex-row items-center">
+                    <div class="flex flex-col items-end mx-2">
+                        <p class="text-lg font-bold text-primary-color">Coordinator</p>
+                        <p class="text-sm text-secondary-color">coordinator@cmb.ac.lk</p>
+                    </div>
+                    <img src="<?= BASE_URL ?>/public/images/icons/user_profile.png" alt="user icon">
+                </div>
             </div>
-
+            <div class="flex justify-evenly gap-5">
+                <div class="flex flex-col justify-evenly w-full gap-2 text-white">
+                    <div class="btn-primary-color py-5 rounded-md">156</div>
+                    <div class="btn-primary-color py-5 rounded-md">1000</div>
+                    <div class="btn-primary-color py-5 rounded-md">11</div>
+                </div>
+                <div class="flex flex-col bg-white w-full">
+                    <h1>Upcoming Events</h1>
+                    <p>Event 1</p>
+                    <p>Event 2</p>
+                    <p>Event 3</p>
+                </div>
+                <div class="flex flex-col bg-white w-full">
+                    <h1>Top Preforming Groups</h1>
+                    <p>Group 1</p>
+                    <p>Group 2</p>
+                    <p>Group 3</p>
+                </div>
+            </div>
         </div>
 </body>
 
