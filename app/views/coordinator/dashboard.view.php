f20479ca562e09802cb21f4daaa24cf0cca76f13
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-08-14 12:40:20 +0530

    add event 4

--- app/views/coordinator/dashboard.view.php
+++ app/views/coordinator/dashboard.view.php
@@ -76,6 +76,7 @@
                     <p>Event 1</p>
                     <p>Event 2</p>
                     <p>Event 3</p>
+                    <p>Event 4</p>
                 </div>
                 <div class="flex flex-col bg-white w-full">
                     <h1>Top Preforming Groups</h1>
