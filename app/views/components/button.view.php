f725963d809ee91203eecbc08a817e6315ec6fee
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-08-16 00:51:07 +0530

    Add button component and render it in home view

---
+++ app/views/components/button.view.php
@@ -0,0 +1,5 @@
+<!--Button Component -->
+
+<button class="btn-primary-color rounded-3xl text-center text-white text-base font-medium px-10 py-2">
+    <?= $componentData['text'] ?>
+</button>
--- core/Controller.php
+++ core/Controller.php
@@ -2,9 +2,14 @@
 
 trait controller
 {
-    public function render($view)
+    public function render($view, $pageData = [])
     {
         $controller = lcfirst(get_class($this));
         require_once "app/views/$controller/$view.view.php";
+    }
+
+    public function renderComponent($component, $componentData = [])
+    {
+        require "app/views/components/$component.view.php";
     }
 }
