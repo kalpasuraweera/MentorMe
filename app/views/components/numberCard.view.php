e63f982b96535e8da289c2adc4f913c694c6bb06
kalpa madhushan <kalpamadhushan18@gmail.com>
2024-09-03 02:27:11 +0530

    chore: Update number card and dashboard styles

--- app/views/components/numberCard.view.php
+++ app/views/components/numberCard.view.php
@@ -1,6 +1,6 @@
 <!-- Dashboard Number Card -->
 
-<div class="<?= $componentData['bg'] ?? 'bg-white text-black' ?> rounded-2xl py-4 px-5 gap-2 flex flex-col justify-center" style="width:200px;">
+<div class="<?= $componentData['bg'] ?? 'bg-white text-black' ?> rounded-2xl py-4 px-5 gap-2 flex flex-col justify-center shadow" style="width:200px;">
     <p class="text-md"><?= $componentData['title'] ?></p>
     <p class="text-lg font-bold"><?= $componentData['value'] ?></p>
 </div>
--- app/views/supervisor/dashboard.view.php
+++ app/views/supervisor/dashboard.view.php
@@ -22,13 +22,64 @@
                     <img src="<?= BASE_URL ?>/public/images/icons/user_profile.png" alt="user icon">
                 </div>
             </div>
-            <div class="flex justify-evenly gap-5">
-                <div class="flex flex-col justify-evenly w-full gap-2 text-white">
+            <div class="flex justify-evenly gap-5 mt-5">
+                <div class="flex flex-col justify-evenly gap-2 text-white">
                     <?php $this->renderComponent('numberCard', ['title' => 'Pending Requests', 'value' => 100, 'bg' => 'btn-primary-color']) ?>
                     <?php $this->renderComponent('numberCard', ['title' => 'Tasks Created', 'value' => 100]) ?>
                     <?php $this->renderComponent('numberCard', ['title' => 'Tasks Completed', 'value' => 100]) ?>
                 </div>
-              
+                <div class="flex flex-col py-5 px-10 text-white bg-white shadow rounded-xl" style="width:300px;">
+                    <p class="text-lg font-bold text-primary-color mb-4">Upcoming Events</p>
+                    <div class="flex flex-col gap-5">
+                        <div class="flex flex-col px-2" style="border-left: 5px solid #4318ff;">
+                            <p class="text-black font-bold">Bi-Weekly Meeting</p>
+                            <p class="text-secondary-color">24 Aug 2021</p>
+                        </div>
+                        <div class="flex flex-col px-2" style="border-left: 5px solid #ff1843;">
+                            <p class="text-black font-bold">Mentorship Session</p>
+                            <p class="text-secondary-color">30 Aug 2021</p>
+                        </div>
+                        <div class="flex flex-col px-2" style="border-left: 5px solid #18ff43;">
+                            <p class="text-black font-bold">Training Session</p>
+                            <p class="text-secondary-color">15 Sep 2021</p>
+                        </div>
+                    </div>
+                    <div class="flex justify-end mt-5">
+                        <p class="text-primary-color font-bold">View All</p>
+                    </div>
+                </div>
+                <div class="flex flex-col py-5 px-10 text-white bg-white shadow rounded-xl" style="width:300px;">
+                    <p class="text-lg font-bold text-primary-color mb-4">Top Performers</p>
+                    <div class="flex flex-col gap-4">
+                        <div class="flex items-center">
+                            <img src="<?= BASE_URL ?>/public/images/icons/user_profile.png" alt="user icon" width="40"
+                                height="40">
+                            <div class="flex flex-col px-2">
+                                <p class="text-black font-bold">John Doe</p>
+                                <p class="text-secondary-color">CS001</p>
+                            </div>
+                        </div>
+                        <div class="flex items-center">
+                            <img src="<?= BASE_URL ?>/public/images/icons/user_profile.png" alt="user icon" width="40"
+                                height="40">
+                            <div class="flex flex-col px-2">
+                                <p class="text-black font-bold">John Doe</p>
+                                <p class="text-secondary-color">CS001</p>
+                            </div>
+                        </div>
+                        <div class="flex items-center">
+                            <img src="<?= BASE_URL ?>/public/images/icons/user_profile.png" alt="user icon" width="40"
+                                height="40">
+                            <div class="flex flex-col px-2">
+                                <p class="text-black font-bold">John Doe</p>
+                                <p class="text-secondary-color">CS001</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-end mt-5">
+                        <p class="text-primary-color font-bold">View All</p>
+                    </div>
+                </div>
             </div>
         </div>
 </body>
