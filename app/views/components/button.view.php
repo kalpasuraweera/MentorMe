95301c757c756fb468749ce3aeb8e70501941b5f
kalpa madhushan <kalpamadhushan18@gmail.com>
2024-09-07 15:06:16 +0530

    button bg

--- app/views/components/button.view.php
+++ app/views/components/button.view.php
@@ -1,6 +1,7 @@
 <!--Button Component -->
 
-<button class="btn-primary-color rounded-3xl text-center text-white text-base font-medium px-10 py-2"
+<button
+    class="<?= $componentData['bg'] ?? 'btn-primary-color' ?> rounded-3xl text-center text-white text-base font-medium px-10 py-2"
     onclick="<?= $componentData['onClick'] ?? '' ?>" name="<?= $componentData['name'] ?? '' ?>"
     type="<?= $componentData['type'] ?? '' ?>">
     <?= $componentData['text'] ?? '' ?>
