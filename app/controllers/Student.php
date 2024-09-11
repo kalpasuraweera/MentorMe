81d97466e76f5e14bdb50c14d8147b5331441bbd
Thamindu Wijerathne <140165111+Thamindu-wijerathne@users.noreply.github.com>
2024-09-11 15:50:31 +0530

    Create student sidebard component

--- app/controllers/Student.php
+++ app/controllers/Student.php
@@ -3,6 +3,38 @@
 class Student
 {
     use controller;
+    public $SidebarMenu = [
+        [
+            'text' => 'Dashboard',
+            'url' => '/student/dashboard',
+            'icon' => 'dashboard'
+        ],
+        [
+            'text' => 'Calender',
+            'url' => '/student/calender',
+            'icon' => 'dashboard'
+        ],
+        [
+            'text' => 'Tasks',
+            'url' => '/student/tasks',
+            'icon' => 'dashboard'
+        ],
+        [
+            'text' => 'Schedules',
+            'url' => '/student/schedules',
+            'icon' => 'dashboard'
+        ],
+        [
+            'text' => 'Settings',
+            'url' => '/student/settings',
+            'icon' => 'dashboard'
+        ],
+        [
+            'text' => 'Logout',
+            'url' => '/auth/logout',
+            'icon' => 'dashboard'
+        ]
+    ];
 
     public function index($data)
     {
