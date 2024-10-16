1d30412fdaf542313bf3a36327eb4243fd60df07
kalpa madhushan <kalpamadhushan18@gmail.com>
2024-10-16 16:29:00 +0530

    add leader options

--- app/controllers/Student.php
+++ app/controllers/Student.php
@@ -36,6 +36,50 @@ class Student
         ]
     ];
 
+    public function __construct()
+    {
+        // Add Leader Options if the user is a student leader
+        if (isset($_SESSION['user']['role']) && $_SESSION['user']['role'] === 'STUDENT_LEADER') {
+            $this->SidebarMenu = [
+                [
+                    'text' => 'Dashboard',
+                    'url' => '/student/dashboard',
+                    'icon' => 'dashboard'
+                ],
+                [
+                    'text' => 'Calender',
+                    'url' => '/student/calender',
+                    'icon' => 'dashboard'
+                ],
+                [
+                    'text' => 'Tasks',
+                    'url' => '/student/tasks',
+                    'icon' => 'dashboard'
+                ],
+                [
+                    'text' => 'Schedules',
+                    'url' => '/student/schedules',
+                    'icon' => 'dashboard'
+                ],
+                [
+                    'text' => 'Settings',
+                    'url' => '/student/settings',
+                    'icon' => 'dashboard'
+                ],
+                [
+                    'text' => 'Leader Options',
+                    'url' => '/student/leader',
+                    'icon' => 'dashboard'
+                ],
+                [
+                    'text' => 'Logout',
+                    'url' => '/auth/logout',
+                    'icon' => 'dashboard'
+                ]
+            ];
+        }
+    }
+
     public function index($data)
     {
         $this->render("dashboard");
@@ -65,5 +109,9 @@ class Student
     public function supervisorData($data)
     {
         $this->render("supervisorData");
+    }
+    public function leader($data)
+    {
+        $this->render("leader");
     }
 }
