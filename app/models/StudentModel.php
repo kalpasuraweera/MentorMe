8cba08a49789f73bc7f7b9818a6574ab02b73e11
kalpa madhushan <kalpamadhushan18@gmail.com>
2024-11-03 17:30:14 +0530

    add model functions to update and delete

--- app/models/StudentModel.php
+++ app/models/StudentModel.php
@@ -15,4 +15,25 @@ class StudentModel
         //TODO: Later we have to get meeting requests also
         return $this->execute($query, $data);
     }
+
+    // Update a Request
+    public function updateGroupRequest($data)
+    {
+        $query = "
+        UPDATE supervisor_request
+        SET project_title = :project_title, idea = :idea, reason = :reason
+        WHERE request_id = :request_id
+        ";
+        return $this->execute($query, $data);
+    }
+
+    // Delete a Request
+    public function deleteGroupRequest($data)
+    {
+        $query = "
+        DELETE FROM supervisor_request
+        WHERE request_id = :request_id
+        ";
+        return $this->execute($query, $data);
+    }
 }
