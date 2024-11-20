87ccfe6979971f2e08f9da20392447aeb6213186
kalpa madhushan <kalpamadhushan18@gmail.com>
2024-11-20 14:56:27 +0530

    add supervisor feedbacks

--- app/models/FeedbackModel.php
+++ app/models/FeedbackModel.php
@@ -4,21 +4,38 @@ class FeedbackModel
 {
     use Model;
     protected $table = "feedback";
-    public function getUserFeedbacks($data)
+    public function getExaminerFeedbacks($data)
     {
         $query = "
             SELECT * FROM `feedback`
-            WHERE user_id = :user_id AND group_id = :group_id
+            WHERE user_id = :user_id AND group_id = :group_id AND type = 'EXAMINER_FEEDBACK'
         ";
         return $this->execute($query, $data);
     }
 
-    public function addUserFeedback($data)
+    public function addExaminerFeedback($data)
     {
         $query = "
-            INSERT INTO `feedback` (user_id, group_id, feedback)
-            VALUES (:user_id, :group_id, :feedback)
-            ON DUPLICATE KEY UPDATE feedback = :feedback
+            INSERT INTO `feedback` (user_id, group_id, feedback, type)
+            VALUES (:user_id, :group_id, :feedback, 'EXAMINER_FEEDBACK')
+        ";
+        return $this->execute($query, $data);
+    }
+
+    public function getSupervisorFeedbacks($data)
+    {
+        $query = "
+            SELECT * FROM `feedback`
+            WHERE user_id = :user_id AND group_id = :group_id AND type = 'SUPERVISOR_FEEDBACK'
+        ";
+        return $this->execute($query, $data);
+    }
+
+    public function addSupervisorFeedback($data)
+    {
+        $query = "
+            INSERT INTO `feedback` (user_id, group_id, feedback, type)
+            VALUES (:user_id, :group_id, :feedback, 'SUPERVISOR_FEEDBACK')
         ";
         return $this->execute($query, $data);
     }
