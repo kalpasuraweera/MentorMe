67545e8f4341609c49afa952db67ecb9a610738d
kalpa madhushan <kalpamadhushan18@gmail.com>
2024-11-22 09:45:24 +0530

    align button

--- app/views/student/requestSuperVisor.view.php
+++ app/views/student/requestSuperVisor.view.php
@@ -38,10 +38,10 @@
                 <!-- Hidden input for supervisor id -->
                 <input type="hidden" name="supervisor_id" id="supervisor_id">
                 <div class="flex justify-end gap-5">
-                    <button type="submit"
-                        class="btn-primary-color rounded-3xl text-center text-white text-base font-medium px-10 py-2">Request</button>
                     <button
                         class="btn-secondary-color rounded-3xl text-center text-white text-base font-medium px-10 py-2 closeRequestPopup">Cancel</button>
+                    <button type="submit"
+                        class="btn-primary-color rounded-3xl text-center text-white text-base font-medium px-10 py-2">Request</button>
                 </div>
             </div>
         </form>
