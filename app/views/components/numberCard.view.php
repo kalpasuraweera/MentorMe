072d29e297225ee4699afe3b60077f52262ae89f
kalpa madhushan <kalpamadhushan18@gmail.com>
2024-11-27 10:32:11 +0530

    style home page

--- app/views/components/numberCard.view.php
+++ app/views/components/numberCard.view.php
@@ -1,11 +1,11 @@
 <!-- Dashboard Number Card -->
 
-<div class="<?= $componentData['bg'] ?? 'bg-white text-black' ?> rounded-2xl py-4 px-5 gap-2 flex shadow">
+<div class="<?= $componentData['bg'] ?? 'bg-white text-black' ?> rounded-2xl py-4 px-5 gap-4 flex items-center shadow">
     <?php if (isset($componentData['icon'])): ?>
         <img src="<?= BASE_URL ?>/public/images/icons/<?= $componentData['icon'] ?>" alt="user icon" width="40" height="40">
     <?php endif; ?>
-    <div class="flex flex-col justify-center" style="width:200px;">
+    <div class="flex flex-col gap-2 justify-center" style="width:200px;">
         <p class="text-md"><?= $componentData['title'] ?></p>
-        <p class="text-lg font-bold"><?= $componentData['value'] ?></p>
+        <p class="text-xl font-bold"><?= $componentData['value'] ?></p>
     </div>
 </div>
