f91ae694b0ec1fd1c5f28238e031fb93b1cb9f6b
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-04-12 16:10:13 +0530

    add mail function

--- core/init.php
+++ core/init.php
@@ -5,6 +5,9 @@ spl_autoload_register(function ($className) {
     require_once __DIR__ . "/../app/models/" . $className . ".php";
 });
 
+// Include the Mail class
+require_once __DIR__ . "/Mail.php";
+
 require_once "config.php";
 require_once "App.php";
 require_once "Controller.php";
