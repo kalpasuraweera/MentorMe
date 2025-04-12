9037f4f6460cbda0cc7a6bdbf172e23cc8db67b2
Thihansa921 <thinzas921@gmail.com>
2025-04-12 14:02:57 +0530

    Search Students by Index Number

--- app/models/CoordinatorModel.php
+++ app/models/CoordinatorModel.php
@@ -598,5 +598,18 @@ class CoordinatorModel
         ";
         return $this->execute($query);
     }
+
+    public function getStudentByIndexNumber($indexNumber)
+    {
+        $query = "
+        SELECT * FROM student
+        JOIN user ON student.user_id = user.user_id
+        LEFT JOIN bracket ON student.bracket_id = bracket.bracket_id
+        WHERE student.index_number = :index_number
+        ";
+        $params = [':index_number' => $indexNumber];
+        return $this->execute($query, $params);
+    }
+
 }
 
