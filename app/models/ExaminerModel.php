6891b500bc79d4afedc1ada940d58da34550bfe6
TheAkila <nishanakila11@gmail.com>
2025-04-15 17:01:54 +0530

    update

--- app/models/ExaminerModel.php
+++ app/models/ExaminerModel.php
@@ -3,41 +3,51 @@
 class ExaminerModel
 {
     use Model;
-    protected $table = "examiner";
+    protected $table = "examinor";
 
-    public function getExaminerData($data)
+    public function getExaminerData ($data)
     {
-        $query = "
+        $query ="
         SELECT * FROM examiner
-        JOIN user ON examiner.user_id = user.user_id
+        JOIN user ON examiner.user_id=user.user_id
         WHERE examiner.user_id = :user_id
         ";
+
         return $this->execute($query, $data);
     }
 
+
+
     public function updateExaminer($data)
     {
-        $query = "
+        $query ="
         UPDATE examiner
         SET description = :description
         WHERE user_id = :user_id
-        ";
+         ";
+
+
         $queryData = [
             'description' => $data['description'],
-            'user_id' => $data['user_id']
+            'user_id' => $data ['user_id']
         ];
+
         $this->execute($query, $queryData);
+
         $query = "
         UPDATE user
         SET full_name = :full_name, last_update = :last_update
         WHERE user_id = :user_id";
+
         $queryData = [
-            'full_name' => $data['full_name'],
-            'last_update' => $data['last_update'],
-            'user_id' => $data['user_id']
+            'full_name' =>$data['full_name'],
+            'last_update'  => $data['last_update'],
+            'user_id' => $data ['user_id']
         ];
+
         return $this->execute($query, $queryData);
+        
+        
+        
     }
-
-
 }
